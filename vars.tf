76dfdec886a4a39a4887aa5556d405cf24c3fad5
juppad23 <juppad23@in.ibm.com>
2020-02-17 14:10:24 +0530

    Changed files for terraform 12 sleepy

--- vars.tf
+++ vars.tf
@@ -2,10 +2,11 @@
 
 variable "sample_var" {
   description = "A sample_var to pass to the template."
-  default     = "hello sir"
+  default     = "hello"
 }
 
 variable "sleepy_time" {
   description = "How long our local-exec will take a nap."
   default     = 0
 }
+
