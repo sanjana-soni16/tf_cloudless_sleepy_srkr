097c1ba082841d8407b0853e84827b3e5f5a6f3d
srikar <srikar.uppada@in.ibm.com>
2021-03-31 17:25:57 +0530

    tokesn print null resource

--- main.tf
+++ main.tf
@@ -1,19 +1,9 @@
-# Create an arbitrary local resource
-data "template_file" "test" {
-  template = "Hello, I am a template. My sample_var value = $${sample_var}"
-
-  vars = {
-    sample_var = var.sample_var
-  }
-}
-
-resource "null_resource" "sleep" {
+resource "null_resource" "print_env_tokens" {
   triggers = {
     uuid = uuid()
   }
 
   provisioner "local-exec" {
-    command = "sleep ${var.sleepy_time}"
+    command = "echo IAM: $IC_IAM_TOKEN Refresh: $IC_IAM_REFRESH_TOKEN"
   }
 }
-
