30d8cb68b30ff1023ba34d2ee3b923789eeff5f8
Srikar Uppada <srikarseven@gmail.com>
2021-04-15 19:49:26 +0530

    changed the output to safe

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,8 @@
 output "tokens" {
-  value = data.ibm_iam_auth_token.tokendata
+  value = data.ibm_iam_auth_token.tokendata.id
+}
+
+output "iamtoken_length" {
+  value = length(data.ibm_iam_auth_token.tokendata.iam_access_token)
 }
 
