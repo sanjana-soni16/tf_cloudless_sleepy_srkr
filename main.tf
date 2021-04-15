dcc2af188d95bdfa7e195dedbca6b87d742660fb
Srikar Uppada <srikarseven@gmail.com>
2021-04-15 19:51:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,1 @@
 data "ibm_iam_auth_token" "tokendata" {}
-
-
-resource "null_resource" "print_env_tokens" {
-  triggers = {
-    uuid = uuid()
-  }
-
-  provisioner "local-exec" {
-    command = "echo IAM: $IC_IAM_TOKEN Refresh: $IC_REFRESH_TOKEN"
-  }
-}
