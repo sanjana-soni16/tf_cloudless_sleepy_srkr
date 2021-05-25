b18ffd610165833b25ce143045af0eb35bb84bd2
Srikar Uppada <srikarseven@gmail.com>
2021-05-25 17:13:57 +0530

    Update version for tf 12 test

--- versions.tf
+++ versions.tf
@@ -1,8 +1,3 @@
-terraform {
-    required_providers {
-        ibm = {
-            source = "IBM-Cloud/ibm"
-            version = "~> 1.22.0"
-        }
-    }
+provider "ibm" {
+    version          = "1.22.0"
 }
